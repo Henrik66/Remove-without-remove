99ab4269c183588a51685f712911ee77ed689854
Henrik Gombos <henrik99999@gmail.com>
2020-08-02 17:27:59 +0000

    User input for number

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 def remove (inputlist, itemtoremove, outputlist):
   whensomethingisremoved = False
-
   for i in inputlist:
     if i == itemtoremove and whensomethingisremoved == False :
-      print ("removing")
       whensomethingisremoved = True
     else:
-      print ("adding", i)
       outputlist.append(i)
 
-n = [1, 2, 3, 4, 5, 6, 7, 8,1]
+n = [1, 2, 3, 4, 5, 6, 7, 8,4, 4]
 output = []
 
-remove (n, 1, output)
+userinput = input("Enter the number you want removed: ")
+usernumber = int(userinput)
+remove (n, usernumber, output)
 print (n)
 print (output)
